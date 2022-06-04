fb08db3badd9e8eadb49880a8f0ac80e1d4f4a1a
Aries <mailto:aries.prasetiyo@digiasia.asia>
2022-06-04 07:57:20 +0700

    Initial Commit

---
+++ 42Race_Business_Apps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  42Race_Business_Apps
+//
+//  Created by Admin on 04/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
